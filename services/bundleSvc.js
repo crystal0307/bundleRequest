568414a15d986b1f8b5196180a53163d02491251
崔青杰 <guoxuan@kakuxuandeMac-mini.local>
2017-03-13 12:59:56 +0800

    update

--- services/bundleSvc.js
+++ services/bundleSvc.js
@@ -1,10 +1,16 @@
-  import * as Promise from 'promise';
-  import { Injectable } from '@angular/core';
-  import { Http } from '@angular/http';
 
-  @Injectable()
-export class BundleSvc {
-  constructor(public http: Http, public global: Global) {
+  const Promise = require('promise');
+  const User = require('../models/User');
 
-}
-}
+  var BundleSvc = function(){
+
+  }
+  BundleSvc.prototype.getBundles = function(){
+    console.log('getaaa');
+    return new Promise((resolve, reject) => {
+       var list = User.aggregate([{$unwind:"$applies"},{$project:{applies:1,_id:0}}]).sort({"applies.time":-1});
+       resolve(list);
+    })
+  }
+
+  module.exports = BundleSvc;
