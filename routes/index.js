b46d97b5673cf624222a378cea132b2d8ca7d3d1
阮晶晶 <1454874676@qq.com>
2017-03-16 14:05:25 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,18 @@ var filter=require("lodash/filter");
 var moment=require("moment");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // var bundleSvc = new BundleSvc();
-  // bundleSvc.getBundles().then(data => {
-  //   var myDate = new Date();
-  //   var nowDate=moment(myDate).locale('zh-cn').format('L');
-  //   var aData=filter(data,(l) => {
-  //      return l.applies.time.substring(0,10)==nowDate;
-  //   })
-  //   res.render('index',{ title: 'f3ff', datas:aData});
-  // })
   getData('todayData').then(data =>{
-    res.render('index',{ title: 'f3ff', datas:data});
+    res.render('index',{datas:data});
+  }).catch((error) =>{
+    //console.log('3');
   })
 });
 
 router.get('/currDate', function(req, res, next){
   getData('todayData').then(data =>{
      res.json({list:data});
+  }).catch((error) =>{
+
   })
 
 })
@@ -29,13 +24,17 @@ router.get('/currDate', function(req, res, next){
 router.get('/allDate', function(req, res, next){
     getData('allDate').then(data =>{
       res.json({listall:data});
+    }).catch((error) =>{
+
     })
 })
 
 function getData(req){
   return new Promise((resolve,reject) =>{
+    //throw new Error("aaaaaaerr");
     var bundleSvc = new BundleSvc();
     bundleSvc.getBundles().then(data =>{
+      // throw new Error("aaaaaaerr");
       if(req=='allDate'){
         resolve(data);
       }else if(req=='todayData'){
@@ -46,7 +45,12 @@ function getData(req){
         })
         resolve(aData);
       }
+    }).catch((err)=>{
+      //console.log('1');
+      reject(err);
     })
+  }).catch((err)=>{
+    reject(err);
   })
 }
 
