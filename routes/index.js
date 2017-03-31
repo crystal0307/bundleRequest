9b1a9fac0704371ead8259542a15a22010c1aaa2
阮晶晶 <1454874676@qq.com>
2017-03-31 09:38:01 +0800

    orderlist

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const BundleSvc = require('../services/bundleSvc.js');
+const OrderSvc=require('../services/orderSvc.js');
 var filter=require("lodash/filter");
 var moment=require("moment");
+var dataType="";
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  dataType = "Bundle";
   getData('todayData').then(data =>{
     res.render('index',{datas:data});
   }).catch((error) =>{
@@ -13,30 +16,51 @@ router.get('/', function(req, res, next) {
       });//解决中文提示乱码问题
     res.end('系统错误，请重试!');
   })
+
+
 });
 
 router.get('/currDate', function(req, res, next){
-  getData('todayData').then(data =>{
-     res.json({list:data});
-  }).catch((error) =>{
-    res.writeHead(200, {
-          "Content-Type": "text/html;charset=utf-8"
-      });
-    res.end('系统错误，请重试!');
+  if(req.query.val != undefined && req.query.val != null){
+    dataType=req.query.val;
+  }
+  if(dataType=="Order"){
+    getOrder('todayOrder').then(data =>{
+      res.json({list:data,flag:"order"});
   })
-
-})
-
-router.get('/allDate', function(req, res, next){
-    getData('allDate').then(data =>{
-      res.json({listall:data});
+}else{
+    getData('todayData').then(data =>{
+      //console.log(data);
+       res.json({list:data,flag:"bundle"});
     }).catch((error) =>{
       res.writeHead(200, {
             "Content-Type": "text/html;charset=utf-8"
         });
       res.end('系统错误，请重试!');
     })
+  }
+
+
 })
+
+router.get('/allDate', function(req, res, next){
+  if(dataType=="Order"){
+    getOrder('allOrder').then(data =>{
+      res.json({listall:data,flag:"order"});
+  })
+}else{
+    getData('allDate').then(data =>{
+      res.json({listall:data,flag:"bundle"});
+    }).catch((error) =>{
+      res.writeHead(200, {
+            "Content-Type": "text/html;charset=utf-8"
+        });
+      res.end('系统错误，请重试!');
+    })
+  }
+
+})
+
 
 function getData(req){
   return new Promise((resolve,reject) =>{
@@ -58,6 +82,24 @@ function getData(req){
     })
   }).catch((err)=>{
     reject(err);
+  })
+}
+function getOrder(req){
+  return new Promise((resolve,reject) =>{
+    var orderSvc=new OrderSvc();
+    orderSvc.getOrders().then(data =>{
+      if(req=='allOrder'){
+        resolve(data);
+      }else if(req=='todayOrder'){
+        var myDate = new Date();
+        var nowDate=moment(myDate).locale('zh-cn').format('L');
+        // console.log(nowDate);
+        var aData=filter(data,(l) => {
+           return l.created.substring(0,10)==nowDate;
+        })
+        resolve(aData);
+      }
+    })
   })
 }
 
--- services/orderSvc.js
+++ services/orderSvc.js
@@ -1,3 +1,42 @@
 /**
  * Created by guoxuan on 17/3/29.
  */
+const Promise=require('promise');
+const Order = require('../models/order');
+const Item=require('../models/item');
+var _=require("lodash");
+
+
+var OrderSvc=function(){
+
+}
+
+
+OrderSvc.prototype.getOrders=function(){
+  return new Promise((resolve,reject) =>{
+   Order.find().then(orders =>{
+    //  console.log(_.filter(orders,(f) =>{return f.address.name == "黄一鸣";}));
+     Item.find().then(items=>{
+       var resList=[];
+        //console.log(resList.length);
+       //items 需要遍历的集合
+       _.forEach(items,(item)=>{
+         var data=_.filter(orders,(o)=>{
+           return o.itemId==item._id;
+         })
+         _.forEach(data,(order)=>{
+           var name=item.name;
+           resList.push({"itemName":name,"quantity":order.quantity,"created":order.created,"province":order.address.province,"city":order.address.city,"detail":order.address.detail,"tel":order.address.tel,"name":order.address.name,"status":order.status});
+          //  resList.push(order);
+         })
+       })
+       resolve(resList);
+     })
+    }).catch(err =>{
+      reject(err);
+    });
+  },err =>{
+    reject(err);
+  })
+}
+module.exports=OrderSvc;
