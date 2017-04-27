0fff7c04883332bb611ce2f9e6605d0ae7e76166
郭轩 <guoxuan@guoxuan.local>
2017-04-27 09:45:02 +0800

    OK

--- global.js
+++ global.js
@@ -3,3 +3,4 @@
  */
 module.exports.APPID = 'wxfa41e36dbf1c347f';
 module.exports.APPSECRET = '56f87e5085200685087e805f2ae361e7';
+module.exports.TEMPLATEID = 'PJM4POFAdchwLkISkz2y_5_ttlcsJSVTcmYDjAuk94g';
